88ed6eabab85ce7160b078c8bcea94e2636a6068
arda9898 <ardademir.1998@outlook.com>
2021-05-30 19:43:01 +0300

    firestore connection done

---
+++ src/app/pages/products/product.model.ts
@@ -0,0 +1,13 @@
+// export class Product {
+//     id: string;
+//     price: number;
+//     pdoductName;
+//     endDate: Date;
+
+// }
+export class Product {
+    id?: string;
+    price?: number;
+    pdoductName?:String;
+    endDate?: Date;
+  }
