e97aa07f8b4b7f5fae88fed71b338cb7f5caa9f8
arda9898 <ardademir.1998@outlook.com>
2021-06-06 20:11:20 +0300

    update product updated

--- src/app/pages/products/product.model.ts
+++ src/app/pages/products/product.model.ts
@@ -1,11 +1,12 @@
-import {Bid} from './bid.model'
 export interface Product {
-    customIdName?: string;
-    price?: number;
-    productName?:String;
-    endDate?: Date;
-    //bids?: Array<Bid>;
-    bider_email:string;
-    bid_amount:number;
+  customIdName?: string;
+  price?: number;
+  productName?: String;
+  endDate?: {
+    seconds: number,
+    nanoseconds: number;
+  };
+  bider_email: string;
+  bid_amount: number;
 
-  }
+}
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 import { ProductServiceService } from './product-service.service';
 import { Product } from './product.model';
 import { FormGroup, FormControl } from '@angular/forms';
@@ -10,34 +10,68 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-items: Observable<any[]>;
-public product?: Product;
-bidForm: FormGroup;
+  items: Observable<any[]>;
+  public product: Product;
+  bidForm: FormGroup;
+  bid: number;
 
+  private subscription: Subscription;
 
-  constructor(public productService:ProductServiceService) {    
+  public dateNow = new Date();
+  public dDay = new Date('Jan 01 2021 00:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute = 60;
+
+  public timeDifference;
+  public secondsToDday;
+  public minutesToDday;
+  public hoursToDday;
+  public daysToDday;
+
+  private getTimeDifference(product:Product) {
+    //console.log(this.dDay);
+    console.log("prod:",this.product);
+     this.dDay = new Date(this.product.endDate.seconds);
+    //console.log(this.dDay);
+    //console.log(this.dDay, new Date(this.product.endDate))
+    this.timeDifference = this.dDay.getTime() - new Date().getTime();
+    this.allocateTimeUnits(this.timeDifference);
   }
-ngOnInit() {
-  this.items = this.productService.getAllDocs();
 
-  this.bidForm = new FormGroup({
+  private allocateTimeUnits(timeDifference) {
+    this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+    this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+    this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
 
-    'bider_email': new FormControl(null),
-    'bid_amount': new FormControl(null),
-  });
+
+  constructor(public productService: ProductServiceService) {
+  }
+  ngOnInit() {
+    this.items = this.productService.getAllDocs();
 
   }
 
-  clickEvent(product){
-    this.productService.updateItem(product);
+  clickEvent(product, bid) {
+    this.productService.updateItem(product, bid);
+    console.log(product);
 
+    this.subscription = interval(1000)
+      .subscribe(x => { this.getTimeDifference(product); });
   }
 
-  // TODO: starting price olacak 
+  // TODO: starting price olacak
   // bid yaptım price artıcak
   // countdown bitince bid i yapan customer a bildirim gidicek
   // time 0 olursa işlem alma
   //nice to have: buy now masaya vuran alır
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
 
